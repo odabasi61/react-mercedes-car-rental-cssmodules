80e395b1c7a462798b363160770a5df1482b5352
Ali ODABAŞI <aliodabas_a@hotmail.com>
2023-03-11 18:25:41 +0300

    navbar css finished

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
 import styles from "./Navbar.module.css";
+import {
+  AiOutlineMenu,
+  AiOutlineClose,
+  AiOutlineSearch,
+  AiOutlineUser,
+} from "react-icons/ai";
+import Logo from "../../images/logo.png";
 
 const Navbar = () => {
-  return <div></div>;
+  const [nav, setNav] = useState(false);
+
+  return (
+    <header className={styles.navbar}>
+      <img src={Logo} alt="drive logo" />
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <a href="/">Learn More</a>
+          </li>
+          <li>
+            <a href="/">Log in</a>
+          </li>
+          <li>
+            <a href="/">Sign up</a>
+          </li>
+          <li>
+            <AiOutlineSearch size={25} style={{ marginTop: "6px" }} />
+          </li>
+          <li>
+            <AiOutlineUser size={25} style={{ marginTop: "6px" }} />
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.mobile_btn}>
+        <AiOutlineMenu size={25} />
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
